707c079115b8d7d3a47a20c8b479511d9b84d9c4
Leonardo <lgsalmeida15@gmail.com>
2026-07-06 22:15:27 -0300

    fix: seed migration use ativo boolean instead of status

--- db/migrations/000022_seed_admin_user.up.sql
+++ db/migrations/000022_seed_admin_user.up.sql
@@ -1,9 +1,9 @@
-INSERT INTO _etl.usuarios (nome, email, password, role, status)
+INSERT INTO _etl.usuarios (nome, email, password, role, ativo)
 VALUES (
   'Leonardo Gabriel',
   'leonardo.gabriel@ejfa.com.br',
   '$2a$10$CNgmtmFGhZRS09QOW9u4Qu.gv9CZ0gAASXkP7bdt3Uu.evyEjh/.2',
   'admin_global',
-  'ativo'
+  true
 )
 ON CONFLICT (email) DO NOTHING;
